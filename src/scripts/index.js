ae37cbdb4244850f0f156c35605c174697f80cb1
dewa nanda <dewananda089@gmail.com>
2022-09-23 08:47:35 +0700

    Responsive humberger-button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
+import '../styles/responsive.scss';
+import './script'
 
 console.log('Hello Coders! :)');
